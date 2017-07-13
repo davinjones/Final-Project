44b50ecb2938452ad8ca44dd293a948789782b09
Davin Jones <davin@davinjones.com>
2017-07-13 13:11:30 -0400

    Finalizing and fine tuning

--- js/script.js
+++ js/script.js
@@ -1,53 +1,14 @@
-
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-var i;
-    var slides =
-        document.getElementsByClassName("mySlides");
-    var captionText = 
-        document.getElementById("caption");
-
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById('myModal').style.display = "none";
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-*/
-
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 var $caption = $("<p></p>");
-
+/* variables */
 $overlay.append($image);
 $overlay.append($caption);
 $("#portfolio").append($overlay);
 $overlay.hide();
 
-$(".col-md-4 a").click(function() {
+/* lightbox jquery */
+$(".col-sm-6 a").click(function() {
     event.preventDefault();
     var href = $(this).attr("href");
     console.log(href);
@@ -62,6 +23,7 @@ $overlay.click(function() {
     $overlay.hide();
 });
 
+/* place holder response in lieu of back-end */
 $(".btn").click(function () {
     event.preventDefault();
     $(".col-md-8").replaceWith("<h1 class='curs submission'>Thank you for your submission!</h1>")
